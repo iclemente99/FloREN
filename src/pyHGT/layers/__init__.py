15f7c729761eb6bf4c503038ede23245301fdd79
iclemente99 <99278194+iclemente99@users.noreply.github.com>
2026-04-27 16:02:43 -0400

    Add files via upload

---
+++ src/pyHGT/layers/__init__.py
@@ -0,0 +1,6 @@
+from .gcn import GCN
+from .readout import AvgReadout
+from .discriminator import Discriminator
+from .discriminator2 import Discriminator2
+from .mlp import MLP
+from .mlp2 import MLP2
